4b27dee18e249611375c7336d786999ce409a99b
Chelsea David <cryan17@cnm.edu>
2018-07-23 09:07:31 -0600

    update with proper function structure

--- Snap.php
+++ Snap.php
@@ -1,4 +1,4 @@
-<?php
+<?php // My way
 function Factorial($n) {
 	$factorial = $n * ($n-1) * ($n-2)* ($n-3) * 2 * 1;
 	for ($i=1; $i <= $n; $i++){
@@ -9,5 +9,35 @@ function Factorial($n) {
 
 ?>
 
+<!-- Dylans way -->
 
+<?php
+function factorial(int $n): int {
+	if($n < 0) {
+		throw(new \RangeException("no negatives - the power of positive thinking!"));
+	}
+	if($n > 20) {
+		throw(new \RangeException("RED ALERT! NUMBERS TOO LARGE! WE NEED TO EJECT THE WARP CORE!"));
+	}
 
+	$answer = 1; // default to 0! = 1
+	for($i = 1; $i <= $n; $i++) {
+		$answer = $answer * $i;
+	}
+	return $answer;
+}
+
+function factorialRecursive(int $n): int {
+	if($n < 0) {
+		throw(new \RangeException("no negatives - the power of positive thinking!"));
+	}
+	if($n > 20) {
+		throw(new \RangeException("RED ALERT! NUMBERS TOO LARGE! WE NEED TO EJECT THE WARP CORE!"));
+	}
+
+	if($n === 0) {
+		return 1;
+	} else {
+		return($n * factorialRecursive($n - 1));
+	}
+}
